be05d06c04a94750ee42a8af5c9964b4a8789a42
touge <touge1027@gmail.com>
2021-09-08 20:02:39 +0800

    Export more types

--- lib/mixswap_sdk_dart.dart
+++ lib/mixswap_sdk_dart.dart
@@ -1,3 +1,13 @@
 library mixswap_sdk_dart;
 
 export 'src/client.dart';
+export 'src/vo/asset.dart';
+export 'src/vo/chain_asset.dart';
+export 'src/vo/exchange.dart';
+export 'src/vo/exchange_order.dart';
+export 'src/vo/mix_response.dart';
+export 'src/vo/order.dart';
+export 'src/vo/rank.dart';
+export 'src/vo/route.dart';
+export 'src/vo/route_data.dart';
+export 'src/vo/source.dart';
